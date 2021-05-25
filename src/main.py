2e2a3ddb6e089f21654b4ffc315cc9278b7fcddb
JamesUNT <thiagoaugusto.inc@gmail.com>
2021-05-25 00:45:07 -0300

    feature: comunicação entre servidor e db

--- src/main.py
+++ src/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 
 # from .dependencies import get_query_token, get_token_header
 # from .internal import admin
-from routers import scan, mp
+from routers import scan, mp, totem
 
 app = FastAPI()
 app.include_router(scan.router)
 app.include_router(mp.router)
+app.include_router(totem.router)
