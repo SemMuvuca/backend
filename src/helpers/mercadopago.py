23182308c84e2f0d816f3268e368168bc57365c5
JamesUNT <thiagoaugusto.inc@gmail.com>
2021-05-26 21:46:26 -0300

    criando helper de criação de ordem

--- src/helpers/mercadopago.py
+++ src/helpers/mercadopago.py
@@ -1,40 +1,58 @@
-import httpx
+# import httpx
 import os
 from datastore import client as db_client
-from queries import simple_query
+from .queries import simple_query
 
 base_url: str = (
     f"https://api.mercadopago.com/instore/qr/seller/collectors/{os.getenv('USER_ID')}"
 )
 create_order_url: str = f"{base_url}/stores/{os.getenv('EXTERNAL_STORE_ID')}/pos/{os.getenv('EXTERNAL_POS_ID')}/orders"
 get_order_url: str = f"{base_url}/pos/{os.getenv('EXTERNAL_POS_ID')}/orders"
+headers = {"Authorization": f"Bearer {os.getenv('ACCESS_TOKEN')}"}
 
-headers = {"Authorization": f"Bearer {os.getenv("ACCESS_TOKEN")}"}
 
-async def create_order():
+async def create_order(products: list):
+    product_list = []
+    total_amount = 0.0
+
+    for item in products:
+        produto = await simple_query(
+            db_client, "Produto", "barcode", "=", item["ean13_code"]
+        )
+        product_list.append(
+            {
+                "title": produto["title"],
+                "description": produto["description"],
+                "category": produto["category"],
+                "unit_measure": produto["unit"],
+                "unit_price": produto["unit_price"],
+                "quantity": item["quantity"],
+                "total_amount": produto["unit_price"] * item["quantity"],
+            }
+        )
+        total_amount += produto["unit_price"] * item["quantity"]
+
     order = {
-    "external_reference": 12345,
-    "title": "Product order",
-    "description": "This is the order",
-    "notification_url": "www.yourserver.com",
-    "total_amount": 100
-    "items": [
-        # {
-        # "sku_number": "A123K9191938",
-        # "category": "marketplace",
-        # "title": "Point Mini",
-        # "description": "This is the Point Mini",
-        # "unit_price": 100,
-        # "quantity": 1,
-        # "unit_measure": "unit",
-        # "total_amount": 100
-        # }
-    ]
+        "external_reference": 12345,
+        "title": "Product order",
+        "description": "This is the order",
+        "notification_url": "https://hookb.in/pza3ZNO01DuXNNqwBgyk",
+        "total_amount": total_amount,
+        "items": product_list,
     }
-    with httpx.Client(headers=headers) as client:
-        return client.put(create_order_url)
+    # with httpx.Client(headers=headers) as client:
+    #     return client.put(create_order_url)
+    print(order)
 
 
 async def status_order():
     with httpx.Client(headers=headers) as client:
         return client.get(create_order_url)
+
+
+create_order(
+    [
+        {"ean13_code": "7891991000833", "quantity": 2},
+        {"ean13_code": "7894900940398", "quantity": 3},
+    ]
+)
