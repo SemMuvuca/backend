63ce5d198e0c90c4554a134502f4bb5701260d78
Daniel Berg <masterberg1hu3@gmail.com>
2021-06-01 13:59:42 -0300

    Using production keys

--- app/services/mercadopago.py
+++ app/services/mercadopago.py
@@ -10,14 +10,13 @@ base_url: str = (
 )
 create_order_url: str = f"{base_url}/stores/{os.getenv('EXTERNAL_STORE_ID')}/pos/{os.getenv('EXTERNAL_POS_ID')}/orders"
 get_order_url: str = f"{base_url}/pos/{os.getenv('EXTERNAL_POS_ID')}/orders"
-headers = {"Authorization": f"Bearer {os.getenv('ACCESS_TOKEN')}"}
 
 
 async def create_order(products):
     product_list = []
     total_amount = 0.0
     for item in products["products_quantity"]:
-        produto = await get_product(item["ean13_code"])
+        produto = get_product(item["ean13_code"])
         product_list.append(
             {
                 "title": produto["title"],
@@ -31,17 +30,33 @@ async def create_order(products):
         )
         total_amount += produto["unit_price"] * item["quantity"]
     order = {
-        "external_reference": 12345,
+        "external_reference": "12345",
         "title": "Product order",
         "description": "This is the order",
-        "notification_url": "https://hookb.in/pza3ZNO01DuXNNqwBgyk",
+        "notification_url": "sem-muvuca.rj.r.appspot.com/mercadopago/ipn",
         "total_amount": total_amount,
         "items": product_list,
     }
+    headers = {
+        "Authorization": "Bearer APP_USR-1005181805798098-032819-0d61694407b2dc0f38266e2dfa533aab-235427759"
+    }
     with httpx.Client(headers=headers) as client:
-        return client.put(url=create_order_url, data=jsonable_encoder(order))
+        headers = {"Content-Type": "application/json"}
+        print(headers)
+        print(create_order_url)
+        print(jsonable_encoder(order))
+        r = client.put(
+            url="https://api.mercadopago.com/instore/qr/seller/collectors/235427759/stores/LOJA1337/pos/CAIXA8080/orders",
+            data=jsonable_encoder(order),
+            headers=headers,
+        )
+        print(r.request.headers)
+        print(r)
 
 
 async def get_status_order():
+    headers = {
+        "Authorization": "Bearer APP_USR-1005181805798098-032819-0d61694407b2dc0f38266e2dfa533aab-235427759"
+    }
     with httpx.Client(headers=headers) as client:
         return client.get(create_order_url)
